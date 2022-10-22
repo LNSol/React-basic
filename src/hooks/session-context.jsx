fc499104e0135a55be01fb8ee5a7c978823ecd7b
EunhaLim <dmsgk1559@gmail.com>
2022-10-22 10:53:23 +0900

    refactoring session-context addCartItem

--- src/hooks/session-context.jsx
+++ src/hooks/session-context.jsx
@@ -1,24 +1,29 @@
 import { createContext, useContext, useState } from 'react';
+import SampleSession from '../../public/data/sample.json';
 
 const SessionContext = createContext();
-const sampleSession = {
-  loginUser: { id: 1, name: 'Hong' },
-  cart: [
-    { id: 100, name: '라면', price: 3000 },
-    { id: 101, name: '컵라면', price: 2000 },
-    { id: 102, name: '파', price: 5000 },
-  ],
-};
+// const sampleSession = {
+//   loginUser: { id: 1, name: 'Hong' },
+//   cart: [
+//     { id: 100, name: '라면', price: 3000 },
+//     { id: 101, name: '컵라면', price: 2000 },
+//     { id: 102, name: '파', price: 5000 },
+//   ],
+// };
 
 export const SessionProvider = ({ children }) => {
-  const [session, setSession] = useState(sampleSession);
+  const [session, setSession] = useState(SampleSession);
 
   const logout = () => setSession({ ...session, loginUser: null });
   const addCartItem = (id, name, price) => {
-    setSession({
-      ...session,
-      cart: session.cart.concat({ id, name, price }),
-    });
+    const maxId = Math.max(...session.cart.map((item) => item.id), 0); // mapBy
+    session.cart.push({ id: maxId + 1, name, price });
+    setSession({ ...session });
+    //bad
+    // setSession({
+    //   ...session,
+    //   cart: session.cart.concat({ id, name, price }),
+    // });
   };
   const removeCartItem = (itemId) => {
     setSession({
